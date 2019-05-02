0ba94f9efd55345c17bc6080b16cff007e91cfb2
Sean Seung Won Lee <swdev1202@gmail.com>
2019-05-02 12:17:14 -0700

    training model

---
+++ train_model.py
@@ -0,0 +1,130 @@
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.optimizers import Adam, RMSprop
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import CSVLogger
+import numpy as np
+import pandas as pd
+
+# TRAIN? 1, TEST? 0
+TRAIN = 0
+
+# first, read training/validation data's directory
+training_data_dir = "data/training"
+validation_data_dir = "data/validation"
+test_data_dir = "data/test"
+
+# hyperparameters
+IMAGE_WIDTH, IMAGE_HEIGHT = 224, 224
+RGB = 3
+EPOCHS = 100
+BATCH_SIZE = 32
+
+# create CNN model
+model = Sequential()
+
+model.add(Conv2D(32,(3,3), padding='same', input_shape=(IMAGE_WIDTH, IMAGE_HEIGHT, RGB), activation='relu'))
+model.add(Conv2D(32,(3,3), padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(64,(3,3), padding='same', activation='relu'))
+model.add(Conv2D(64,(3,3), padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(128,(3,3), padding='same', activation='relu'))
+model.add(Conv2D(128,(3,3), padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(256,(3,3), padding='same', activation='relu'))
+model.add(Conv2D(256,(3,3), padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(3))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer=RMSprop(lr=0.0001), metrics=['accuracy'])
+
+# data augmentation
+training_data_generator = ImageDataGenerator(
+    rescale=1/255,
+    shear_range=0.1,
+    zoom_range=0.1,
+    rotation_range=5,
+    horizontal_flip=True)
+
+validation_data_generator = ImageDataGenerator(
+    rescale=1/225
+)
+
+test_data_generator = ImageDataGenerator(
+    rescale=1/255
+)
+
+# load data using flow (stream of data)
+training_generator = training_data_generator.flow_from_directory(
+    training_data_dir,
+    target_size=(IMAGE_WIDTH, IMAGE_HEIGHT),
+    batch_size=BATCH_SIZE,
+    class_mode='categorical')
+
+validation_generator = validation_data_generator.flow_from_directory(
+    validation_data_dir,
+    target_size=(IMAGE_WIDTH, IMAGE_HEIGHT),
+    batch_size=BATCH_SIZE,
+    class_mode='categorical')
+
+test_generator = test_data_generator.flow_from_directory(
+    test_data_dir,
+    target_size=(IMAGE_WIDTH, IMAGE_HEIGHT),
+    batch_size=1,
+    class_mode="categorical",
+    shuffle=False)
+
+# training
+if(TRAIN):
+    model.fit_generator(
+        training_generator,
+        steps_per_epoch = len(training_generator.filenames) // BATCH_SIZE,
+        epochs=EPOCHS,
+        validation_data=validation_generator,
+        validation_steps=len(validation_generator.filenames) // BATCH_SIZE,
+        verbose=1,
+        callbacks=[CSVLogger('log3.csv', append=False, separator=",")])
+
+    model.save_weights('model.h5')
+# testing
+else:
+    model.load_weights('model_100epoch_no_hor_flip.h5')
+
+    #TEST_FILE = "test_file.txt"
+    #open(TEST_FILE,"w")
+
+    STEP_SIZE_TEST=test_generator.n//test_generator.batch_size
+
+    prediction = model.predict_generator(test_generator, steps=STEP_SIZE_TEST, verbose=1)
+    
+    predicted_class_indices=np.argmax(prediction,axis=1)
+    print(predicted_class_indices)
+
+    labels = (test_generator.class_indices)
+    print(labels)
+    labels = dict((v,k) for k,v in labels.items())
+    predictions = [labels[k] for k in predicted_class_indices]
+
+    filenames=test_generator.filenames
+    results=pd.DataFrame({"Filename":filenames,
+                      "Predictions":predictions})
+    results.to_csv("results.csv",index=False)
+
+    loss, acc = model.evaluate_generator(test_generator, steps=STEP_SIZE_TEST, verbose=1)
+    print(loss)
+    print(acc)
+
+
